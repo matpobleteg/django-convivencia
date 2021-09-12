e97812fed044cf5c8468797c08ba15edbe8e6e2a
Matias Poblete <matias@sudoers.cl>
2021-09-12 10:28:09 -0300

    primer commit: creacion de proyecto, aplicaciones y sus config

---
+++ accounts/tests.py
@@ -0,0 +1,45 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+# Create your tests here.
+
+
+class CustomUserTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='will',
+            email='will@email.com',
+            password='testpass123'
+        )
+        self.assertEqual(user.username, 'will')
+        self.assertEqual(user.email, 'will@email.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='superuser',
+            email='superuser@email.com',
+            password='testpass123'
+        )
+        self.assertEqual(admin_user.username, 'superuser')
+        self.assertEqual(admin_user.email, 'superuser@email.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+
+
+class SignupPageTest(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'registration/signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(self.response, 'Puras hueás')
